d6a99dc31622c45393adde659f8f45d8f793f7ac
JakobHeiter <j.heiter21504@gmail.com>
2024-07-18 13:48:23 +0200

    Add files via upload

--- modules_JH.py
+++ modules_JH.py
@@ -13,9 +13,9 @@ def Tca (r_self, r_enemy, v_self, v_enemy):
 
 def Dtca_abs (tca, r_self, r_enemy, v_self, v_enemy):
     
-    dtca = np.abs(((r_enemy- r_self)- ((v_enemy- v_self)*np.dot(\
-            (r_enemy-r_self),(v_enemy-v_self))/(np.dot((v_enemy-v_self),\
-            (v_enemy-v_self))))))
+    dtca = np.linalg.norm((r_enemy- r_self)- ((v_enemy- v_self)*(np.dot(\
+            (r_enemy-r_self),(v_enemy-v_self)))/(np.dot((v_enemy-v_self),\
+            (v_enemy-v_self)))))
     return dtca
 
 def Dtca_vec (tca, r_self, r_enemy, v_self, v_enemy):
@@ -27,9 +27,14 @@ def Dtca_vec (tca, r_self, r_enemy, v_self, v_enemy):
 
 def Res_acc (tca,  r_self, r_enemy, v_self, v_enemy):
     R_puck = 1.
-    r_tca_self = r_of_t(r_self, v_self,np.array[0,0], tca)
-    r_tca_enemy = r_of_t(r_enemy, v_enemy, np.array[0,0], tca)
+    r_tca_self = r_of_t(r_self, v_self,np.array([0,0]), tca)
+    r_tca_enemy = r_of_t(r_enemy, v_enemy, np.array([0,0]), tca)
     r_tca = r_tca_enemy - r_tca_self
     res_acc = 2*(2*R_puck - r_tca)*(tca**(-2))
     return res_acc
     
+#nur zum testen:
+a = np.array([12,24])
+b = np.array([1,12])
+c = np.array([-12,5])
+d = np.array([23-2])
