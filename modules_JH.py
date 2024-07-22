66068e5e01fcd2d2e36173cdfbadab3c64f7c39b
JakobHeiter <j.heiter21504@gmail.com>
2024-07-22 13:58:43 +0200

    Add files via upload
    Added collision checker (check_rebound) routine

--- modules_JH.py
+++ modules_JH.py
@@ -32,6 +32,28 @@ def Res_acc (tca,  r_self, r_enemy, v_self, v_enemy):
     r_tca = r_tca_enemy - r_tca_self
     res_acc = 2*(2*R_puck - r_tca)*(tca**(-2))
     return res_acc
+
+def danger_check(r_self, r_enemy, v_self, v_enemy):
+    tca = Tca(r_self, r_enemy, v_self, v_enemy)
+    if tca <= 2.0 and Dtca_abs(tca, r_self, r_enemy, v_self, v_enemy) <= 2.5:  #random Zeitwert. Einheit? Passt das? Testen
+        return True
+    else:
+        return False
+
+def check_rebound(r,v,box_x_min, box_x_max, box_y_min, box_y_max, steps):
+    for i in range(steps):
+        x_next = r[0]+v[0]
+        y_next = r[1]+v[1]
+        if x_next<box_x_min or x_next > box_x_max:
+            return x_next
+        if y_next < box_y_min or y_next > box_y_max:
+            return y_next
+        r[0] = x_next
+        r[1] = y_next
+    return False
+        
+    
+    
     
 #nur zum testen:
 a = np.array([12,24])
