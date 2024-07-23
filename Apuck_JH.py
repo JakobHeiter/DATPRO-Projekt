2954a6de27ec2168cbac9b5c11e2897899c525e8
JakobHeiter <j.heiter21504@gmail.com>
2024-07-23 13:50:47 +0200

    Add files via upload
    Erster Teil der dauerhaften Checks hinzugefügt

--- Apuck_JH.py
+++ Apuck_JH.py
@@ -22,7 +22,7 @@ def worker_heiter(id, secret, q_request, q_reply):
     box_ymax = simbox.ymax
     
     pucks = dict()#Zentrales Verzeichnis der Pucks
-    danger_list = []
+    danger_list = []#verzeichnis der Intruder
     D = 2 #Durchmesser der Pucks
     amax = 100.
         
@@ -32,12 +32,12 @@ def worker_heiter(id, secret, q_request, q_reply):
         if puck.is_alive() == False:
             continue
         if puck.get_name()== 'heiter':
-            me = puck                #speichert mich gesondert als me ab
+            me = puck                #speichert mich gesondert als 'me' ab
             continue
         p_list = [puck.get_id(), puck.get_position(), puck.get_velocity(), \
                   puck.get_acceleration(), puck.get_time(), puck.is_alive()]
         pucks(i = p_list)
-    
+        
     for i in range(len(pucks)):#Prüft welche Pucks gefährlich werden könnten und setzt diese auf die danger_list
         tca = Tca(me.get_position(),pucks(i[1]),me.get_velocity(),pucks(i[2]))
         if tca < 10:#random Zahl -> testen
@@ -50,12 +50,29 @@ def worker_heiter(id, secret, q_request, q_reply):
                 #time.sleep(2/50) #-> dann kann man halt in der Zeit nichts anderes machen -> threading, asyncio
                 q_request.put('SET_ACCELERATION', 0, secret, id)
                 danger_list.pop(-1) #den Puck für den ausgewichen wurde streichen
+            
+    while True:#dauernde checks
+        for i  in range(len(danger_list)):#check der gefährder, timing fehlt
+            q_request.put('GET_PUCK', danger_list[i[0]], id)
+            puck = q_reply.get()[1]
+            if puck.is_alive() == False:
+                continue
+            tca = Tca(me.get_position(),puck.get_position(),me.get_velocity(),puck.get_velocity())
+            if tca >= 10:
+                danger_list.remove[i]
+                continue
+            else:
+                if Dtca_abs(tca,me.get_position(), puck.get_position(), me.get_velocity(),\
+                            puck.get_velocity()) < 1.1 * D:
+                    resacc = Res_acc(tca,me.get_position(), pucks(i[1]),\
+                                     me.get_velocity(),pucks(i[2]))
+                    q_request.put('SET_ACCELERATION', resacc, secret, id)
+                    #time.sleep(2/50??) #-> dann kann man halt in der Zeit nichts anderes machen -> threading, asyncio
+                    q_request.put('SET_ACCELERATION', 0, secret, id)
+                    danger_list.remove[i] #den Puck für den ausgewichen wurde streichen
                 
-    #to do: 2 check loops: Für alle und für die Prios (time verwenden)
+            
+            
+    #to do: 2 check loops: Für alle und für die Prios (time/threading/asyncio verwenden)
     #auch: reflexion an rand checken
-    #      geschwindigkeit nach Ausweichen checken und ggf. anderes Ausweichen
-    
-                       
-        
-        
-        
+    #geschwindigkeit nach Ausweichen checken und ggf. anderes Ausweichen       
